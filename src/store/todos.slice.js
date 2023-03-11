e3cbaddd15c505ea3598a7a914b92c11c6f22818
MongieLee <le30n@outlook.com>
2023-03-11 17:48:08 +0800

    feat: 使用extraReducers执行异步操作

--- src/store/todos.slice.js
+++ src/store/todos.slice.js
@@ -1,16 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const TODO_KEY = "todos";
 
-export const loadTodos = createAsyncThunk("todos/loadTodos", (payload, thunkApi) => {
+export const loadTodos = createAsyncThunk("todos/loadTodos", (payload) => {
   console.log(payload);
-  setTimeout(() => {
-    console.log('====================================');
-    console.log("模拟异步操作");
-    console.log('====================================');
-    const tempData = [{ id: Math.random() * 1000, name: "a1" }, { id: Math.random() * 1000, name: "b1" }, { id: Math.random() * 1000, name: "c1" }]
-    thunkApi.dispatch(setTodos(tempData));
-  }, 1000);
+  // const tempData = [{ id: Math.random() * 1000, name: "a1" }, { id: Math.random() * 1000, name: "b1" }, { id: Math.random() * 1000, name: "c1" }]
+  return axios.get("http://localhost:8888/data").then(res => res.data);
 })
 
 const { reducer: TodosReducer, actions } = createSlice({
@@ -35,8 +31,16 @@ const { reducer: TodosReducer, actions } = createSlice({
       console.log(action.payload);
       action.payload.forEach(i => state.push(i))
     }
+  },
+  extraReducers: {
+    [loadTodos.fulfilled]: (state, action) => {
+      console.log(action);
+      console.log('fulfilled');
+      action.payload.forEach(i => state.push(i))
+    }
   }
 });
+
 
 export const { addTodo, setTodos } = actions;
 export default TodosReducer;
